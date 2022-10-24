ffd808f38080dada06b3c9397b5bd84f144cb512
Kimi Loeffel <kimi.loeffel@bit.admin.ch>
2022-10-24 22:00:35 +0200

    test commit

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
 func main() {
+	out, err := exec.Command("git", "log", "--format='%at'").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(out))
 }
