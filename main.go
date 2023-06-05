51d2f8d696f30dd1dbc36e594538b55cb72879c2
Kimi Loeffel <kimi.loeffel@bit.admin.ch>
2023-06-05 14:55:30 +0200

    refactor

--- main.go
+++ main.go
@@ -11,22 +11,17 @@ import (
 	"time"
 )
 
+const delimiter = "  "
+
 func main() {
 	times, err := getTimes()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	perHour := make([]int, 24)
+	hours := make([]int, 24)
 	for _, t := range times {
-		perHour[t.Local().Hour()]++
-	}
-
-	max := 0
-	for _, cnt := range perHour {
-		if cnt > max {
-			max = cnt
-		}
+		hours[t.Local().Hour()]++
 	}
 
 	termWidth, err := getWidth()
@@ -34,24 +29,28 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	max := max(hours)
 	log10 := int(math.Ceil(math.Log10(float64(max))))
 
-	delimiter := "  "
 	// every thing is double escaped because we need to determine the format string first.
 	// afterwards the formatString looks something like this:
 	// "%02d: %3d%s\n"
 	//                                    vv------- this is set by log10
 	formatString := fmt.Sprintf("%%02d: %%%dd%%s\n", log10)
-	// minus 1 because of the newline
-	prefixLen := len(fmt.Sprintf(formatString, 23, max, delimiter)) - 1
 
+	// minus 1 because of the newline
+	prefixLen := float64(len(fmt.Sprintf(formatString, 23, max, delimiter)) - 1)
 	// Max width should be 80
-	width := math.Min(float64(termWidth-prefixLen), float64(80-prefixLen))
+	width := math.Min(float64(termWidth), float64(80)) - prefixLen
 
 	// must have at least one hashtag
 	scaleFactor := math.Max(width/float64(max), 0)
 
-	for hour, amount := range perHour {
+	printHours(hours, formatString, scaleFactor)
+}
+
+func printHours(hours []int, formatString string, scaleFactor float64) {
+	for hour, amount := range hours {
 		numberOfHashtags := int(math.Round(float64(amount) * scaleFactor))
 		hashtags := delimiter + strings.Repeat("#", numberOfHashtags)
 		if scaleFactor == 0 {
@@ -100,4 +99,15 @@ func getWidth() (int, error) {
 	width := splitted[1]
 
 	return strconv.Atoi(string(width))
+}
+
+func max(slice []int) int {
+	max := 0
+	for _, cnt := range slice {
+		if cnt > max {
+			max = cnt
+		}
+	}
+
+	return max
 }
